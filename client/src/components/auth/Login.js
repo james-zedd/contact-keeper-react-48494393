ed97d9571f6f7c775b4ec419d7af63f9f99a1aed
james-zedd <james_zedd@hotmail.com>
2019-12-17 07:02:25 -0500

    update with proxy error

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,9 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState, useContext, useEffect } from "react";
+import AlertContext from "../../context/alert/alertContext";
+import AuthContext from "../../context/auth/authContext";
 
-export const Login = () => {
+const Login = props => {
+  const alertContext = useContext(AlertContext);
+  const authContext = useContext(AuthContext);
+
+  const { setAlert } = alertContext;
+  const { login, error, clearErrors, isAuthenticated } = authContext;
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      props.history.push("/");
+    }
+
+    if (error === "Invalid Credentials") {
+      setAlert(error, "danger");
+      clearErrors();
+    }
+    // eslint-disable-next-line
+  }, [error, isAuthenticated, props.history]);
+
   const [user, setUser] = useState({
-    email: '',
-    password: ''
+    email: "",
+    password: ""
   });
 
   const { email, password } = user;
@@ -11,12 +31,18 @@ export const Login = () => {
   const onChange = e => setUser({ ...user, [e.target.name]: e.target.value });
 
   const onSubmit = e => {
-    e.preventDefault();
-    console.log('Login Submit');
-  }
-  
+    if (email === "" || password === "") {
+      setAlert("Please fill in all fields", "danger");
+    } else {
+      login({
+        email,
+        password
+      });
+    }
+  };
+
   return (
-    <div className='form-container'>
+    <div className="form-container">
       <h1>
         Account <span className="text-primary">Login</span>
       </h1>
@@ -27,12 +53,21 @@ export const Login = () => {
         </div>
         <div className="form-group">
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" value={password} onChange={onChange} />
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={onChange}
+          />
         </div>
-        <input type="submit" value="Login" className="btn btn-primary btn-block" />
+        <input
+          type="submit"
+          value="Login"
+          className="btn btn-primary btn-block"
+        />
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
